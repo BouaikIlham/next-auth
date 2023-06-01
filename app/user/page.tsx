e41e61b6b5e446aba01602ebc30ec543f1e8f2ba
ilham <ibouaik@gmail.com>
2023-06-01 23:00:42 +0100

    text color

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center p-5 green-red-500 text-lg font-bold">
+    <div className="flex justify-center items-center p-5 text-green-500 text-lg font-bold">
       only for users
     </div>
   )
